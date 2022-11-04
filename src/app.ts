e0604f45d2ba757b1a64fcc3251087cd62cab84e
RicardoCaselati <ricardocaselati@gmail.com>
2022-11-04 18:39:31 -0300

    Req.03 e 07 - ok

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import productRoutes from './routes/product.routes';
+import userRoutes from './routes/user.routes';
 
 const app = express();
 
@@ -7,5 +8,6 @@ app.use(express.json());
 
 // Criar rotas aqui
 app.use('/products', productRoutes);
+app.use('/users', userRoutes);
 
 export default app;
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -7,3 +7,19 @@ export interface IProduct {
 export interface Product extends IProduct {
   id: number;
 }
+
+export interface UserCredentials {
+  username: string;
+  password: string;
+}
+
+export interface IUser extends UserCredentials {
+  username: string,
+  classe: string,
+  level: number,
+  password: string,
+}
+
+export interface User extends IUser {
+  id: number;
+}
