35acb57e71096dbf08cce874c8ed6dc2e3e2da0e
RicardoCaselati <ricardocaselati@gmail.com>
2022-11-07 17:27:10 -0300

    Req.06 - ok

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import * as ProductController from '../controllers/product.controller';
+import validateProductBody from '../middlewares/product.validation';
 
 const router = Router();
 
 router.get('/', ProductController.getAll.bind(ProductController));
-router.post('/', ProductController.create.bind(ProductController));
+router.post('/', validateProductBody, ProductController.create.bind(ProductController));
 
 export default router;
